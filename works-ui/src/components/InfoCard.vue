7b0b14dce043f5b434a9a3332d2cc8068011dc5e
YeoCheon Yun <ycyun@ycyun.xyz>
2021-07-21 10:24:16 +0900

    admin Workspace페이지 적용 및 admin 페이지 i18n 적용

---
+++ works-ui/src/components/InfoCard.vue
@@ -0,0 +1,64 @@
+<template>
+
+    <div class="resource-details">
+      <div class="resource-details__name">
+        <a-avatar id='oslogo' :size="100" style="background-color: rgba(0,0,0,0); font-size: 20px" >
+          <font-awesome-icon :icon="['fab', 'centos']" :size="'4x'" style="color: #666;" />
+        </a-avatar>
+        <h4>
+          {{ name }}
+        </h4>
+      </div>
+      <a-tag v-for="tag in tags">
+        {{ tag }}
+      </a-tag>
+      <a-tooltip placement="right">
+        <template #title>connect portal</template>
+        <a-button shape="circle" type="dashed">
+          <PaperClipOutlined/>
+        </a-button>
+      </a-tooltip>
+    </div>
+
+    <ADivider />
+    <AEmpty />
+</template>
+
+<script>
+export default {
+  name: "InfoCard",
+  props:{
+    name: String,
+    tags: Array,
+  }
+}
+</script>
+
+<style  lang="scss" scoped>
+
+.resource-details {
+  text-align: center;
+  margin-bottom: 20px;
+
+  &__name {
+    display: flex;
+    align-items: center;
+    align-content: center;
+    text-align: center;
+
+    .ant-avata {
+      margin-right: 20px;
+      overflow: hidden;
+      min-width: 50px;
+      cursor: pointer;
+      img {
+        height: 100%;
+        width: 100%;
+      }
+    }
+    h4{
+      font-size: 18px;
+    }
+  }
+}
+</style>
---
+++ works-ui/src/components/TabbedContent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="ContentTab">
+    <a-tabs type="card" v-model:activeKey="activeKey" :tab-position="tabPosition">
+      <a-tab-pane key="1" tab="Tab 1"><TableContent :data="data" :columns="columns" /></a-tab-pane>
+      <a-tab-pane key="2" tab="Tab 2"><ListView /></a-tab-pane>
+      <a-tab-pane key="3" tab="Tab 3">Content of Tab Pane 3</a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script>
+import TableContent from "./TableContent";
+
+import { defineComponent, ref } from 'vue';
+import ListView from "@/components/ListView";
+import {data, columns} from "@/data"
+export default defineComponent({
+  components: {ListView, TableContent},
+  setup() {
+    const tabPosition = ref('top');
+    const activeKey = ref('1');
+    return {
+      tabPosition,
+      activeKey,
+      data,
+      columns
+    };
+  },
+});
+</script>
+
+<style>
+#ContentTab{
+  text-align: left;
+}
+</style>
