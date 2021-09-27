568554b12d9438778f0d5722add4e1fcecabf152
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2021-09-27 16:26:36 +0900

    axios 호출 공통부분 수정( 인터셉터 이용해  토큰 삽입하는 로직 추가)

--- works-ui/src/api/index.js
+++ works-ui/src/api/index.js
@@ -1,28 +1,65 @@
+import store from "@/store/index.js";
 import axios from "axios";
-// import store from "../store/index.js";
 
-const worksApi = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-  headers: {
-    Authorization: "works" + localStorage.getItem("token"),
-  },
-});
+function setInterceptors(instance) {
+  instance.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      config.headers.Authorization = "works" + (sessionStorage.getItem("token") == "" ? store.state.user.token : sessionStorage.getItem("token"));
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+  // Add a response interceptor
+  instance.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function (error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      return Promise.reject(error);
+    }
+  );
+  return instance;
+}
+
+function worksApiAuth() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+    withCredentials: true,
+  });
+  return setInterceptors(instance);
+}
+export const worksApi = worksApiAuth();
+
+// const worksApi = axios.create({
+//   baseURL: process.env.VUE_APP_API_URL,
+//   withCredentials: true,
+//   headers: {
+//     Authorization: "works" + store.state.user.token == null ? localStorage.getItem("token") : store.state.user.token,
+//   },
+// });
 
 // function axiosLogin(userData) {
-//   return worksApi.post("/api/login", userData, { withCredentials: true });
+//   return worksApi.post("/api/login", userData);
 // }
 
 // function axiosTokenAuth() {
-//   return worksApi.get("/api/v1/token", { withCredentials: true });
+//   return worksApi.get("/api/v1/token");
 // }
 
 function axiosLogout() {
-  return worksApi.get("/api/v1/logout", { withCredentials: true });
+  return worksApi.get("/api/v1/logout");
 }
 
 export {
-  worksApi,
-  // axiosLogin,
   axiosLogout,
-  // axiosTokenAuth
+  // axiosLogin,
+  // axiosTokenAuth,
 };
