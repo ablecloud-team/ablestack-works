3dff018b9c6fdbc12d0b4d889e53947659aee3d7
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2021-09-29 13:30:31 +0900

    메뉴별 삭제버튼 구분하기 위한 로직 수정 유저 추가 후 vm할당 로직 추가

--- works-ui/src/components/Actions.vue
+++ works-ui/src/components/Actions.vue
@@ -6,7 +6,7 @@
       <a-button shape="circle" @click="setCircleButtonModal('start')">
         <CaretRightOutlined />
       </a-button>
-    </a-tooltip>    
+    </a-tooltip>
     <!--Stop-->
     <a-tooltip v-if="state.buttonBoolean.stop" placement="top">
       <template #title>{{ $t("tooltip.stop") }}</template>
@@ -15,39 +15,36 @@
       </a-button>
     </a-tooltip>
     <!--reset -->
-    <a-tooltip v-if="state.buttonBoolean.reset" placement="top">
-      <template #title>{{ $t("tooltip.reset") }}</template>
-      <a-button shape="circle" @click="setCircleButtonModal('reset')">
-        <ReloadOutlined />
+    <a-tooltip v-if="state.buttonBoolean.userAllocate" placement="top">
+      <template #title>{{ $t("tooltip.userAllocate") }}</template>
+      <a-button shape="circle" @click="setCircleButtonModal('userAllocate')">
+        <UserAddOutlined />
+      </a-button>
+    </a-tooltip>
+    <a-tooltip v-if="state.buttonBoolean.userUnlock" placement="top">
+      <template #title>{{ $t("tooltip.userUnlock") }}</template>
+      <a-button shape="circle" @click="setCircleButtonModal('userUnlock')">
+        <UserDeleteOutlined />
       </a-button>
     </a-tooltip>
     <!--reinstall-->
     <a-tooltip v-if="state.buttonBoolean.reinstall" placement="top">
       <template #title>{{ $t("tooltip.reinstall") }}</template>
-      <a-button
-        shape="circle"
-        @click="setCircleButtonModal('reinstall')"
-      >
+      <a-button shape="circle" @click="setCircleButtonModal('reinstall')">
         <SyncOutlined />
       </a-button>
     </a-tooltip>
     <!--snapshot-->
     <a-tooltip v-if="state.buttonBoolean.snapshot" placement="top">
       <template #title>{{ $t("tooltip.snapshot") }}</template>
-      <a-button
-        shape="circle"
-        @click="setCircleButtonModal('snapshot')"
-      >
+      <a-button shape="circle" @click="setCircleButtonModal('snapshot')">
         <CameraOutlined />
       </a-button>
     </a-tooltip>
     <!--volsnapshot-->
     <a-tooltip v-if="state.buttonBoolean.volsnapshot" placement="top">
       <template #title>{{ $t("tooltip.volsnapshot") }}</template>
-      <a-button
-        shape="circle"
-        @click="setCircleButtonModal('volsnapshot')"
-      >
+      <a-button shape="circle" @click="setCircleButtonModal('volsnapshot')">
         <i class="fas fa-camera-retro"></i>
         <VideoCameraAddOutlined />
       </a-button>
@@ -55,10 +52,7 @@
     <!--isoattach-->
     <a-tooltip v-if="state.buttonBoolean.isoattach" placement="top">
       <template #title>{{ $t("tooltip.isoattach") }}</template>
-      <a-button
-        shape="circle"
-        @click="setCircleButtonModal('isoattach')"
-      >
+      <a-button shape="circle" @click="setCircleButtonModal('isoattach')">
         <PaperClipOutlined />
       </a-button>
     </a-tooltip>
@@ -72,18 +66,42 @@
     <!--pause-->
     <a-tooltip v-if="state.buttonBoolean.pause" placement="top">
       <template #title>{{ $t("tooltip.pause") }}</template>
-      <a-button shape="circle" @click="setCircleButtonModal('pause')">
+      <a-button shape="circle" @click="asdf()">
         <PauseOutlined />
       </a-button>
     </a-tooltip>
-    <!--destroy-->
-    <a-tooltip v-if="state.buttonBoolean.destroy" placement="top">
+    <!--workspaceDestroy-->
+    <a-tooltip v-if="state.buttonBoolean.workspaceDestroy" placement="top">
       <template #title>{{ $t("tooltip.destroy") }}</template>
       <a-button
         type="primary"
         shape="circle"
         danger
-        @click="setCircleButtonModal('destroy')"
+        @click="setCircleButtonModal('workspaceDestroy')"
+      >
+        <DeleteFilled />
+      </a-button>
+    </a-tooltip>
+    <!--vmDestroy-->
+    <a-tooltip v-if="state.buttonBoolean.vmDestroy" placement="top">
+      <template #title>{{ $t("tooltip.destroy") }}</template>
+      <a-button
+        type="primary"
+        shape="circle"
+        danger
+        @click="setCircleButtonModal('vmDestroy')"
+      >
+        <DeleteFilled />
+      </a-button>
+    </a-tooltip>
+    <!--accountDestroy-->
+    <a-tooltip v-if="state.buttonBoolean.accountDestroy" placement="top">
+      <template #title>{{ $t("tooltip.destroy") }}</template>
+      <a-button
+        type="primary"
+        shape="circle"
+        danger
+        @click="setCircleButtonModal('accountDestroy')"
       >
         <DeleteFilled />
       </a-button>
@@ -91,13 +109,42 @@
 
     <!-- Confirm Modal -->
     <a-modal
-      :title="$t('tooltip.'+modalTitle)"
+      :title="$t('tooltip.' + modalTitle)"
       :visible="confirmModalView"
+      :ok-text="$t('label.ok')"
+      :cancel-text="$t('label.cancel')"
       @cancel="handleCancel"
-      @ok="handleSubmit(actionFrom, name, uuid)">
+      @ok="handleSubmit(actionFrom, workspace, uuid)"
+    >
       <p>{{ $t(modalConfirm) }}</p>
     </a-modal>
 
+    <a-modal
+      :title="$t('tooltip.desktop.allocate.user')"
+      v-model:visible="userAllocateVmModalBoolean"
+      width="400px"
+      :ok-text="$t('label.ok')"
+      :cancel-text="$t('label.cancel')"
+      @ok="putUserAllocateVm(workspace, uuid)"
+    >
+      <a-select
+        v-model:value="selectedUser"
+        :placeholder="$t('tooltip.user')"
+        show-search
+        style="width: 100%; margin-top: 7px"
+        option-filter-prop="label"
+        class="addmodal-aform-item-div"
+      >
+        <a-select-option
+          v-for="option in workspaceUserDataList"
+          :key="option.name"
+          :value="option.name"
+          :label="option.name"
+        >
+          {{ option.name }}
+        </a-select-option>
+      </a-select>
+    </a-modal>
   </a-space>
 </template>
 
@@ -119,12 +166,17 @@ export default defineComponent({
       requires: false,
       default: "",
     },
-    name: {
+    workspace: {
       type: String,
       requires: false,
       default: "",
     },
     status: {
+      type: String,
+      requires: false,
+      default: "",
+    },
+    allocateStatus: {
       type: String,
       requires: false,
       default: "",
@@ -135,17 +187,23 @@ export default defineComponent({
     //console.log("==================== props.actionFrom ====================:::: "+props.actionFrom);
     const state = reactive({
       callComponent: ref(props.actionFrom),
+      workspace: ref(props.workspace),
+      uuid: ref(props.uuid),
+      allocateStatus: ref(props.allocateStatus),
       status: ref(props.status),
       buttonBoolean: {
         showModal: ref(false),
         start: ref(false),
         stop: ref(false),
-        reset: ref(false),
+        userAllocate: ref(false),
+        userUnlock: ref(false),
         reinstall: ref(false),
         snapshot: ref(false),
         volsnapshot: ref(false),
         iso: ref(false),
-        destroy: ref(false),
+        workspaceDestroy: ref(false),
+        vmDestroy: ref(false),
+        accountDestroy: ref(false),
         edit: ref(false),
         pause: ref(false),
       },
@@ -154,18 +212,21 @@ export default defineComponent({
       if (state.callComponent === "WorkspaceList" || state.callComponent === "WorkspaceDetail") {
         state.buttonBoolean.start = true;
         state.buttonBoolean.stop = true;
-        state.buttonBoolean.destroy = true;
-      } else if (state.callComponent === "VirtualMachine") {
+        state.buttonBoolean.workspaceDestroy = true;
       } else if (state.callComponent === "VirtualMachineList" || state.callComponent === "VirtualMachineDetail") {
         if (state.status === "Running") {
           state.buttonBoolean.stop = true;
         } else {
           state.buttonBoolean.start = true;
         }
-        state.buttonBoolean.iso = true;
-        state.buttonBoolean.destroy = true;
+        if (state.allocateStatus == "") {
+          state.buttonBoolean.userAllocate = true;
+        } else {
+          state.buttonBoolean.userUnlock = true;
+        }
+        state.buttonBoolean.vmDestroy = true;
       } else if (state.callComponent === "AccountList" || state.callComponent === "AccountDetail") {
-        state.buttonBoolean.destroy = true;
+        state.buttonBoolean.accountDestroy = true;
       } else if (state.callComponent === "GroupPolicyList" || state.callComponent === "GroupPolicyDetail") {
         state.buttonBoolean.destroy = true;
       }
@@ -173,42 +234,109 @@ export default defineComponent({
     return {
       size: ref(sizeValue),
       confirmModalView: ref(false),
+      userAllocateVmModalBoolean: ref(false),
+      workspaceUserDataList: ref([]),
       modalTitle: ref(""),
       modalConfirm: ref(""),
       popView: ref(true),
       state,
     };
   },
-  // emits: [
-  //   'changeAddModal',
-  // ],
+  data() {
+    return {
+      selectedUser: ref(""),
+    }
+  },
+  created() {
+    this.fetchData();
+  },
   methods: {
+    fetchData: function () {
+      if (this.state.callComponent === "VirtualMachineList" ) {
+        //해당 워크스페이스에 추가 된 사용자 목록 조회
+        worksApi
+          .get("/api/v1/group/" + this.state.workspace)
+          .then((response) => {
+            if (response.status == 200) {
+              //console.log(response.data.result.member);
+              const temp =
+                response.data.result.member == undefined
+                  ? ""
+                  : response.data.result.member;
+              for (let str of temp) {
+                this.workspaceUserDataList.push({ name: str.split(",")[0].split("CN=")[1] });
+              }
+            } else {
+              //message.error(this.t("message.response.data.fail"));
+              //console.log(response.message);
+            }
+          })
+          .catch(function (error) {
+            message.error(error);
+            //console.log(error);
+          });
+      }
+      
+    },
+    putUserAllocateVm: function (workspace, uuid) {
+      //console.log(this.selectedUser + "  ::  " + uuid + "  :: " + workspace);
+      let params = new URLSearchParams();
+      params.append("uuid", uuid);
+      params.append("username", this.selectedUser);
+      worksApi
+        .post("/api/v1/instance", params)
+        .then((response) => {
+          if (response.status === 200) {
+            message.success(this.$t("message.user.vm.allocated.completed"), 3);
+            setTimeout(() => {
+              this.$emit("fetchData", "desktop");
+            }, 1000);
+            this.handleCancel();
+          } else {
+            message.error("message.user.vm.allocated.fail");
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
     setCircleButtonModal: function (value) {
-      this.confirmModalView = true;
-      this.popView = false;
-      this.modalTitle = value;
-      if(value == "start") this.modalConfirm = "modal.confirm.start";
-      if(value == "stop") this.modalConfirm = "modal.confirm.stop";
-      if(value == "destroy") this.modalConfirm = "modal.confirm.destroy";
-
+      if (value == "userAllocate") {
+        this.userAllocateVmModalBoolean = true;
+      } else {
+        this.confirmModalView = true;
+        this.popView = false;
+        this.modalTitle = value;
+      }
+      if (value == "start") this.modalConfirm = "modal.confirm.start";
+      if (value == "stop") this.modalConfirm = "modal.confirm.stop";
+      if (value == "workspaceDestroy") this.modalConfirm = "modal.confirm.workspaceDestroy";
+      if (value == "vmDestroy") this.modalConfirm = "modal.confirm.vmDestroy";
+      if (value == "accountDestroy") this.modalConfirm = "modal.confirm.accountDestroy";
+      if (value == "userUnlock") this.userUnlock = "modal.confirm.userUnlock";
     },
     handleCancel: function () {
       this.confirmModalView = false;
+      this.userAllocateVmModalBoolean = false;
     },
-    handleSubmit: function (actionFrom, name, uuid) { //actionFrom, name, uuid
-      console.log(this.modalTitle + "  ::  " +actionFrom + " ::  " + name + " :: "+ uuid);
-      if(actionFrom == "VirtualMachineList"){
+    handleSubmit: function (actionFrom, workspace, uuid) {
+      //console.log(this.modalTitle + "  ::  " + actionFrom + " ::  " + workspace + " :: " + uuid);
+      
+      if (actionFrom == "VirtualMachineList") {
         let worksUrl, resMessage = "";
-        if(this.modalTitle.includes("start")){
+        if (this.modalTitle.includes("start")) {
+          message.loading(this.$t("message.vm.status.starting"), 6);
           worksUrl = "/api/v1/instance/VMStart/" + uuid;
           resMessage = this.$t("message.vm.status.update");
         }
-        if(this.modalTitle.includes("stop")){
+        if (this.modalTitle.includes("stop")) {
+          message.loading(this.$t("message.vm.status.stopping"), 6);
           worksUrl = "/api/v1/instance/VMStop/" + uuid;
           resMessage = this.$t("message.vm.status.update");
         }
-        if(this.modalTitle.includes("destroy")){
-          worksUrl = "/api/v1/instance/VMDestroy/"+ uuid;
+        if (this.modalTitle.includes("vmDestroy")) {
+          message.loading(this.$t("message.vm.status.destroying"), 6);
+          worksUrl = "/api/v1/instance/VMDestroy/" + uuid;
           resMessage = this.$t("message.vm.status.delete");
         }
         worksApi
@@ -216,27 +344,21 @@ export default defineComponent({
           .then((response) => {
             if (response.status == 200) {
               this.vmDataList = response.data.result.list;
-              message.loading(resMessage);
-            } else {
-              message.error("message.vm.status.update.fail");
-            }
-            setTimeout(() => {
-              this.$emit("fetchData");
               this.handleCancel();
-            }, 1000);
-
+              setTimeout(() => {
+                message.destroy();
+                message.success(resMessage);
+                this.$emit("fetchData", "desktop");
+              }, 6000);
+            } else {
+              message.error(this.$t("message.vm.status.update.fail"));
+            }
           })
           .catch(function (error) {
             message.error(error);
           });
-
       }
-      setTimeout(() => {
-        this.$emit("fetchData");
-        this.handleCancel();
-      }, 1000);
- 
-    } 
+    },
   },
 });
 </script>
