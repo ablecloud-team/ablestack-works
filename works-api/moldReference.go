d5d598c301e5c2385ad96c5219478b3291bd8b76
DESKTOP-R3LRC29\user <smlee@ablecloud.io>
2021-10-22 14:42:28 +0900

    VDI 생성시 user_data 데이터 변경

--- works-api/moldReference.go
+++ works-api/moldReference.go
@@ -135,21 +135,27 @@ func getDeployVirtualMachine(workspaceUuid string, instanceUuid string, instance
 	VmName := displayName
 	InstanceUuid := instanceUuid
 	Type := instanceType
+	//"Invoke-WebRequest -Uri http://10.1.1.1/latest/user-data >> \"c:\\agent\\userdata.txt\"\n" +
 	payload := "<powershell>\n" +
-		"date > \"c:\\agent\\installed.txt\"" +
-		"$dnsip = \"" + SambaIP + "\"\n" + "" +
-		"set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter |Select-Object InterfaceAlias, interfaceindex).interfaceindex -ServerAddresses " + WorksIP + "\n" +
+		"date > \"c:\\agent\\installed.txt\"\n" +
+		"$dnsip = \"" + SambaIP + "\"\n" +
+		"echo $dnsip >> \"c:\\agent\\installed.txt\"\n" +
+		"set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter |Select-Object InterfaceAlias, interfaceindex).interfaceindex -ServerAddresses " + SambaIP + "\n" +
 		"$password = \"Ablecloud1!\" | ConvertTo-SecureString -asPlainText -Force\n" +
 		"$username = \"$" + MyDomain + "\\administrator\" \n" +
 		"$credential = New-Object System.Management.Automation.PSCredential($username,$password)\n" +
+		"echo Rename-Computer >> \"c:\\agent\\installed.txt\"\n" +
 		"Rename-Computer -NewName " + VmName + "\n" +
+		"echo Add-Computer >> \"c:\\agent\\installed.txt\"\n" +
 		"Add-Computer -DomainName " + SambaDomain + " -Credential $credential -NewName " + VmName + "\n" +
+		"echo Add-Computer end>> \"c:\\agent\\installed.txt\"\n" +
 		"$conf = '{\"WorksServer\": \"" + WorksIP + "\", \"WorksPort\": " + WorksPort + ", \"Type\": \"" + Type + "\", \"UUID\": \"" + InstanceUuid + "\"}'\n" +
 		"echo $conf| Out-File -Encoding ascii \"c:\\agent\\conf.json\"\n" +
 		"echo $conf\n" +
+		"echo $conf >> \"c:\\agent\\installed.txt\"\n" +
 		"C:\\agent\\nssm.exe set \"Ablecloud Works Agent\" start SERVICE_DELAYED_AUTO_START\n" +
 		"C:\\agent\\nssm.exe restart \"Ablecloud Works Agent\"\n" +
-		"date >> \"c:\\agent\\installed.txt\"" +
+		"date >> \"c:\\agent\\installed.txt\"\n" +
 		"</powershell>"
 	params := []MoldParams{
 		{"command": "deployVirtualMachine"},
@@ -183,7 +189,9 @@ func getDeployVirtualMachine(workspaceUuid string, instanceUuid string, instance
 		res["message"] = "Mold 와 통신에 실패했습니다.(getDeployVirtualMachine)"
 		res["status"] = BaseErrorCode
 	} else {
-		updateWorkspaceTemplateCheck(workspaceInfo.Uuid, AgentCheck)
+		if instanceType == WorkspaceString {
+			updateWorkspaceTemplateCheck(workspaceInfo.Uuid, AgentCheck)
+		}
 		json.NewDecoder(resp.Body).Decode(&res)
 		res["status"] = http.StatusOK
 	}
