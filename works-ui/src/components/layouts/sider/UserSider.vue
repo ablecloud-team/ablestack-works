228115b6ce428bc1434926b3a35988542c8f8f27
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2021-11-03 17:47:28 +0900

    user side파일 재정의

--- works-ui/src/components/layouts/sider/UserSider.vue
+++ works-ui/src/components/layouts/sider/UserSider.vue
@@ -1,26 +1,32 @@
 <template>
-  <div style="width: 256px">
-    <Logo />
-    <a-menu mode="inline" theme="light" style="padding-top: 14px">
-      <a-menu-item key="1">
+  <div style="width: 100%; height: 100%">
+    <Logo @click="$router.push({ name: 'UserDesktop' }); selectedKeysSetting(2)" />
+    <a-menu 
+      v-model:selectedKeys="state.selectedKeys"
+      mode="inline"
+      theme="light"
+      inline="true"
+      style="padding-top: 14px"
+      >
+      <a-menu-item key="1" @click="$router.push({ name: 'Favorite' }); selectedKeysSetting(1)">
         <template #icon>
           <StarOutlined />
         </template>
         <span>즐겨찾기</span>
       </a-menu-item>
-      <a-menu-item key="2" @click="$router.push({ name: 'UserDesktop' })">
+      <a-menu-item key="2" @click="$router.push({ name: 'UserDesktop' }); selectedKeysSetting(2)">
         <template #icon>
           <DesktopOutlined />
         </template>
         <span>데스크탑</span>
       </a-menu-item>
-      <a-menu-item key="3">
+      <a-menu-item key="3" @click="$router.push({ name: 'UserDesktop' }); selectedKeysSetting(3)">
         <template #icon>
           <AppstoreAddOutlined />
         </template>
         <span>애플리케이션</span>
       </a-menu-item>
-      <a-menu-item key="4">
+      <a-menu-item key="4" @click="$router.push({ name: 'UserDesktop' }); selectedKeysSetting(4)">
         <template #icon>
           <CoffeeOutlined />
         </template>
@@ -30,11 +36,46 @@
   </div>
 </template>
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, reactive, ref, watch } from "vue";
 import Logo from "./Logo";
 export default defineComponent({
   components: {
     Logo,
+  },
+  props: {
+    collapsed: Boolean,
+  },
+  emits: ["changeToggleCollapsed"],
+  setup(props) {
+    const state = reactive({
+      collapsed: ref(props.collapsed),
+      selectedKeys: [""],
+      openKeys: [""],
+      preOpenKeys: [""],
+    });
+    watch(
+      () => state.openKeys,
+      (val, oldVal) => {
+        // console.log(val);
+        // console.log(oldVal);
+        state.preOpenKeys = oldVal;
+      }
+    );
+    return {
+      state,
+    };
+  },
+  created() {
+    this.updateMenu();
+  },
+  methods: {
+    updateMenu() {
+      this.selectedKeys = [ "" ? "1" : sessionStorage.getItem("menukey") ];
+    },
+    selectedKeysSetting: function (key) {
+      sessionStorage.setItem("menukey", key);
+      //this.selectedKeys = [key];
+    },
   },
 });
 </script>
