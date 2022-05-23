ad53590b2a84ab169319a610f23cb4f3580dc8e4
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2022-05-23 17:25:23 +0900

    젠킨스 빌드시 빌드정보 표시를 위한 스크립트 수정

--- works-ui/src/components/layouts/footer/Footer.vue
+++ works-ui/src/components/layouts/footer/Footer.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="footer">
     <div class="line">Copyright (c) 2021-2022, ABLECLOUD.Co.Ltd</div>
-    <div class="line">
-      ABLESTACK Bronto(v{{
-        buildVersion + "-" + buildDate + "-" + buildRelease + buildDev
-      }})
-    </div>
+    <div class="line">ABLESTACK({{ buildVersion }})</div>
   </div>
 </template>
 
@@ -14,11 +10,20 @@ export default {
   name: "Footer",
   data() {
     return {
-      buildDate: process.env.VUE_APP_BUILD_DATE,
       buildVersion: process.env.VUE_APP_VERSION,
-      buildRelease: process.env.VUE_APP_RELEASE,
-      buildDev: process.env.VUE_APP_API_URL == "" ? "" : "-dev",
     };
+  },
+  created() {
+    //젠킨스를 이용한 빌드가 아닐 경우 버전 및 빌드 날짜 표시하기 위함.
+    if (this.buildVersion === "") {
+      let version = "Bronto-v2.0.0";
+      let m = new Date();
+      let date =
+        m.getFullYear() +
+        ("0" + (m.getMonth() + 1)).slice(-2) +
+        ("0" + m.getDate()).slice(-2);
+      this.buildVersion = version + "-" + date + "-dev";
+    }
   },
 };
 </script>
